f322264a81e221ae331e5e4e1bf46b1a9e88dac8
AbdToughoj <toughoj1@gmail.com>
2023-07-21 01:31:13 +0300

    class 26

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import HomeView,AboutView
-
 urlpatterns = [
     path('', HomeView.as_view(),name = 'home'),
     path('about-us', AboutView.as_view(),name = 'about'),
